93457e69f75e832fd5e58957032d27454b1336e2
Egor.Vasyakov@waveaccess.ru <egor.vasyakov@waveaccess.ru>
2022-10-07 12:47:32 +0300

    frontend - Создана заготовка фронта

---
+++ frontend/src/main/resources/static/pages/dictionary/dictionary.js
@@ -0,0 +1,2 @@
+angular.module('home-organizer').controller('dictionaryController', function ($scope, $http, $routeParams, $localStorage, $location, API_SERVER) {
+})
---
+++ frontend/src/main/resources/static/pages/home/home.js
@@ -0,0 +1,2 @@
+angular.module('home-organizer').controller('homeController', function ($scope, $http, $routeParams, $localStorage, $location, API_SERVER) {
+})
