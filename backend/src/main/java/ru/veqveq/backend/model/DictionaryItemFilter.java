8c52c726bdd42a360ca1dcd41017866fc44314c0
Egor.Vasyakov@waveaccess.ru <egor.vasyakov@waveaccess.ru>
2022-10-24 16:26:49 +0300

    backend - Фильтр+сортировка

--- backend/src/main/java/ru/veqveq/backend/model/DictionaryItemFilter.java
+++ backend/src/main/java/ru/veqveq/backend/model/DictionaryItemFilter.java
@@ -2,11 +2,10 @@ package ru.veqveq.backend.model;
 
 import lombok.Data;
 
-import java.util.List;
 import java.util.Map;
 
 @Data
 public class DictionaryItemFilter {
-    private List<String> commonFilters;
+    private String commonFilter;
     private Map<String, Object> fieldFilters;
 }
--- backend/src/main/java/ru/veqveq/backend/util/ElasticUtils.java
+++ backend/src/main/java/ru/veqveq/backend/util/ElasticUtils.java
@@ -1,11 +1,13 @@
 package ru.veqveq.backend.util;
 
 import lombok.experimental.UtilityClass;
-import org.elasticsearch.common.unit.Fuzziness;
+import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.util.CollectionUtils;
+import ru.veqveq.backend.dto.item.OutputDictionaryItemDto;
 import ru.veqveq.backend.model.DictionaryItemFilter;
 import ru.veqveq.backend.model.entity.Dictionary;
 import ru.veqveq.backend.model.entity.DictionaryField;
@@ -17,38 +19,46 @@ import java.util.stream.Collectors;
 
 @UtilityClass
 public class ElasticUtils {
-    public void addCommonFilters(BoolQueryBuilder builder, Dictionary dictionary, List<String> commons) {
+    public void addCommonFilters(BoolQueryBuilder builder, Dictionary dictionary, String commonFilter) {
         String[] fieldNames = getTextFields(dictionary, UUID::toString).toArray(new String[0]);
-        if (!CollectionUtils.isEmpty(commons)) {
-            Set<String> tokens = commons.stream()
-                    .map(common -> {
-                        String commonStr = common.trim().replaceAll("\\s+", " ");
-                        return Set.of(commonStr.split("\\s"));
-                    })
-                    .reduce((strings, strings2) -> {
-                        strings.addAll(strings2);
-                        return strings;
-                    })
-                    .orElse(Collections.emptySet());
+        BoolQueryBuilder commonBuilder = new BoolQueryBuilder();
+        if (StringUtils.isNotBlank(commonFilter)) {
+            Set<String> tokens = Set.of(commonFilter.trim().replaceAll("\\s+", " ").split("\\s"));
             for (String token : tokens) {
-                builder.should(QueryBuilders.multiMatchQuery(token, fieldNames).fuzziness(Fuzziness.AUTO));
                 for (String fieldName : fieldNames) {
-                    builder.must(QueryBuilders.matchPhrasePrefixQuery(fieldName, token));
+                    BoolQueryBuilder fieldBuilder = new BoolQueryBuilder();
+                    fieldBuilder.must(QueryBuilders.matchPhrasePrefixQuery(fieldName, token));
+                    fieldBuilder.must(QueryBuilders.matchPhrasePrefixQuery(fieldName, commonFilter));
+                    commonBuilder.should(fieldBuilder);
                 }
             }
+            builder.must(commonBuilder);
         }
     }
 
-    public void addFieldFilters(BoolQueryBuilder builder, Map<String, Object> filters) {
+    public void addFieldFilters(BoolQueryBuilder builder, Dictionary dictionary, Map<String, Object> filters) {
         if (!CollectionUtils.isEmpty(filters)) {
-            filters.forEach((key, value) -> builder.must(QueryBuilders.matchPhrasePrefixQuery(key, value)));
+            List<String> textFields = getTextFields(dictionary, UUID::toString);
+            List<String> notTextFields = getNotTextFields(dictionary, UUID::toString);
+            filters.forEach((key, value) -> {
+                if (textFields.contains(key)) {
+                    builder.must(QueryBuilders.matchPhrasePrefixQuery(key, value));
+                } else if (notTextFields.contains(key)) {
+                    builder.must(QueryBuilders.matchQuery(key, value));
+                }
+            });
         }
     }
 
     public HighlightBuilder addHighlight(Dictionary dictionary, DictionaryItemFilter filter) {
         HighlightBuilder builder = new HighlightBuilder();
+        builder.requireFieldMatch(false);
+        builder.preTags("<b class= \"text-red-600\">");
+        builder.postTags("</b>");
+        builder.highlighterType("fvh");
+        builder.boundaryScannerType(HighlightBuilder.BoundaryScannerType.CHARS);
         Set<HighlightBuilder.Field> highlightFields = new HashSet<>();
-        if (!CollectionUtils.isEmpty(filter.getCommonFilters())) {
+        if (StringUtils.isNotBlank(filter.getCommonFilter())) {
             highlightFields.addAll(getTextFields(dictionary, val -> new HighlightBuilder.Field(val.toString())));
         }
         if (!CollectionUtils.isEmpty(filter.getFieldFilters())) {
@@ -58,11 +68,30 @@ public class ElasticUtils {
         return builder;
     }
 
+    public OutputDictionaryItemDto getHitValue(SearchHit searchHit) {
+        OutputDictionaryItemDto dto = new OutputDictionaryItemDto();
+        dto.setId(UUID.fromString(searchHit.getId()));
+        dto.setFieldValues(searchHit.getSourceAsMap());
+        if (!searchHit.getHighlightFields().isEmpty()) {
+            searchHit.getHighlightFields().forEach((k, v) -> dto.getFieldValues().put(k, StringUtils.join(v.fragments())));
+        }
+        return dto;
+    }
 
-    private <V> List<V> getTextFields(Dictionary dictionary, Function<UUID, V> mappingFunction) {
+
+    public <V> List<V> getTextFields(Dictionary dictionary, Function<UUID, V> mappingFunction) {
         return dictionary.getFields()
                 .stream()
                 .filter(field -> field.getType() == DictionaryFieldType.Text)
+                .map(DictionaryField::getId)
+                .map(mappingFunction)
+                .collect(Collectors.toList());
+    }
+
+    private <V> List<V> getNotTextFields(Dictionary dictionary, Function<UUID, V> mappingFunction) {
+        return dictionary.getFields()
+                .stream()
+                .filter(field -> field.getType() != DictionaryFieldType.Text)
                 .map(DictionaryField::getId)
                 .map(mappingFunction)
                 .collect(Collectors.toList());
